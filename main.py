3d1daad76128b225d9438a4709a8c48cb3621b85
jstarr25 <jeff.catibog@outlook.com>
2024-07-13 19:29:28 +0100

    Added function to convert dictionary to multiple dictionaries per letter

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
      file_contents = read_file(path)
      total_words = count_words(file_contents)
      letters = letter_count(file_contents)
-     print(letters)
+     letter_list = dict_to_list(letters)
+     print(letter_list)
     
 def read_file(path):
     with open(path) as f:
@@ -23,5 +24,9 @@ def letter_count(file_contents):
         else:
             letters[letter] = 1
      return letters
-    
+
+def dict_to_list(letters):
+    lists = [{"letter": letter, "count": count} for letter, count in letters.items()]
+    return lists
+
 main()
